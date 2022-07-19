15a2adffb9d5cf6a1a790650ebeaf3ca3485bdea
Délita Makanda <delita.makanda@gmail.com>
2022-07-19 20:39:04 +0200

    feat: signin page #wip

---
+++ lib/pages/auth/signin_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class SigninPage extends StatelessWidget {
+  const SigninPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var emailController = TextEditingController();
+    var passwordController = TextEditingController();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+          child: Column(
+        children: [],
+      )),
+    );
+  }
+}
